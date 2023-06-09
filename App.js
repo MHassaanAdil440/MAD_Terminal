65b702d740bd430db54915af9b1a59f80376aa72
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-06-09 15:50:47 +0500

    dikane ke kabil commit

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, BackHandler } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/database';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  BackHandler,
+  Modal,
+  TextInput,
+  Platform,
+  Button,
+  Dimensions,
+} from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
 
 // Initialize Firebase app
 const firebaseConfig = {
@@ -25,12 +37,18 @@ const HomeScreen = () => {
   const [lastKey, setLastKey] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isFetching, setIsFetching] = useState(false);
+  const [osName, setOsName] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalText, setModalText] = useState("");
 
   useEffect(() => {
     fetchCars();
 
     // Add event listener for Back button press
-    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      handleBackPress
+    );
 
     // Cleanup the event listener on component unmount
     return () => backHandler.remove();
@@ -39,12 +57,12 @@ const HomeScreen = () => {
   const fetchCars = () => {
     setIsLoading(true);
 
-    let query = database.ref('cars').orderByKey();
+    let query = database.ref("cars").orderByKey();
     if (lastKey) {
       query = query.startAfter(String(lastKey));
     }
 
-    query.limitToFirst(10).once('value', (snapshot) => {
+    query.limitToFirst(10).once("value", (snapshot) => {
       const data = snapshot.val();
       if (data) {
         const carArray = Object.values(data);
@@ -58,13 +76,35 @@ const HomeScreen = () => {
   };
 
   const handleItemPress = () => {
-    navigation.navigate('Dashboard');
+    navigation.navigate("Dashboard");
   };
 
   const handleBackPress = () => {
     // Kill the app instead of going back
     BackHandler.exitApp();
     return true;
+  };
+
+  const handleCheckOS = () => {
+    const os = Platform.OS;
+    setOsName(os);
+  };
+
+  const storeOSName = (name) => {
+    // Get a reference to the "osNames" node in the database
+    const osNamesRef = database.ref("osNames");
+
+    // Push a new entry with the OS name to the database
+    osNamesRef.push(name, (error) => {
+      if (error) {
+        // Handle the error
+        console.log("Failed to save OS name:", error);
+      } else {
+        // OS name saved successfully
+        setModalText("Saved");
+        setModalVisible(true);
+      }
+    });
   };
 
   const renderItem = ({ item }) => (
@@ -79,6 +119,16 @@ const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>MAD TERMINAL</Text>
+      <Text>Your OS: {osName}</Text>
+      <Button title="Check My OS" onPress={handleCheckOS} />
+      <Modal visible={modalVisible}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>{modalText}</Text>
+            <Button title="Close" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
       {isLoading ? (
         <Text style={styles.loadingText}>Loading...</Text>
       ) : (
@@ -86,7 +136,11 @@ const HomeScreen = () => {
           data={carData}
           renderItem={renderItem}
           keyExtractor={(item) => item.key.toString()}
-          ListFooterComponent={isFetching && <Text style={styles.loadingText}>Loading more...</Text>}
+          ListFooterComponent={
+            isFetching && (
+              <Text style={styles.loadingText}>Loading more...</Text>
+            )
+          }
         />
       )}
     </View>
@@ -104,7 +158,10 @@ const DashboardScreen = () => {
 
   useEffect(() => {
     // Add event listener for Back button press
-    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      handleBackPress
+    );
 
     // Cleanup the event listener on component unmount
     return () => backHandler.remove();
@@ -133,33 +190,36 @@ export default function App() {
   );
 }
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F1F6F9',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F1F6F9",
+    alignItems: "center",
+    justifyContent: "center",
   },
   heading: {
     fontSize: 34,
-    fontWeight: 'bold',
-    color: '#212A3E',
+    fontWeight: "bold",
+    color: "#212A3E",
     marginTop: 50,
     marginBottom: 16,
   },
   loadingText: {
     fontSize: 16,
-    color: '#888',
+    color: "#888",
     marginTop: 16,
   },
   itemContainer: {
     padding: 16,
     borderBottomWidth: 1,
-    borderBottomColor: 'white',
+    borderBottomColor: "white",
   },
   name: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   price: {
     fontSize: 16,
@@ -167,19 +227,33 @@ const styles = StyleSheet.create({
   },
   speed: {
     fontSize: 14,
-    color: '#888',
+    color: "#888",
     marginTop: 4,
   },
   button: {
-    backgroundColor: '#212A3E',
+    backgroundColor: "#212A3E",
     paddingVertical: 12,
     paddingHorizontal: 24,
     borderRadius: 8,
     marginTop: 16,
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    width: windowWidth * 0.4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
   },
 });
